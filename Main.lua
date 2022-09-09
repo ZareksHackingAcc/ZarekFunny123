915928e6a6df236ea946ca6f133caf588388a90b
Zera <101909986+ZareksHackingAcc@users.noreply.github.com>
2022-09-08 19:32:19 -0500

    Add files via upload

---
+++ Main.lua
@@ -0,0 +1,9 @@
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+    love.graphics.setBackgroundColor(255,0,0,0)
+end
---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.title = "Lua"
+    t.width = "1024"
+    t.height = "600"
+end
